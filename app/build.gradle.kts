d87d3676c3c41f2e9fe4e0cbaef0fc9f8c0c5490
@hexfa <amir.dehdarian2009@gmail.com>
2024-07-25 16:55:41 +0330

    Change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.hexfa.weatherapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hexfa.weatherapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
